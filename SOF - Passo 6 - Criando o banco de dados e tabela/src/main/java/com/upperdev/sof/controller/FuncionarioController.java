832b9b39fc268090b5a71a8e63dac1df294dd656
UpperDev - Erik Lima <65689144+UpperDev-Git@users.noreply.github.com>
2020-09-05 02:02:59 -0300

    Vídeo: Passo 6 - Criando o banco de dados e tabela

---
+++ SOF - Passo 6 - Criando o banco de dados e tabela/src/main/java/com/upperdev/sof/controller/FuncionarioController.java
@@ -0,0 +1,22 @@
+package com.upperdev.sof.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FuncionarioController {
+	
+	
+	public FuncionarioController(){
+	}
+
+	//--------------------------------//
+
+	@RequestMapping("/")
+	public String exibirPaginaInicial(){
+		
+		return "inicio";
+		
+	}
+	
+
+}
